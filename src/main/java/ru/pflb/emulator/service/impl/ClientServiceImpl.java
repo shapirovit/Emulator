d43e618dcbfe400a6755420e02a8d3bd366f2c27
shapirovit <56390684+shapirovit@users.noreply.github.com>
2020-09-02 21:37:36 +0300

    add method random getString

--- src/main/java/ru/pflb/emulator/service/impl/ClientServiceImpl.java
+++ src/main/java/ru/pflb/emulator/service/impl/ClientServiceImpl.java
@@ -5,15 +5,18 @@ import org.springframework.stereotype.Service;
 import ru.pflb.emulator.model.dto.ClientDto;
 import ru.pflb.emulator.service.ClientService;
 
+import java.util.Random;
+
 @Service
 public class ClientServiceImpl implements ClientService {
 
 //    @Override
     public ClientDto getClient(long id) {
+
         ClientDto client = ClientDto.builder()
                 .id(id)
-                .firstName("Ivan")
-                .lastName("Petrov")
+                .firstName(this.getString())
+                .lastName(this.getString())
                 .cardNumber("" + Math.round(Math.random() * 987654321))
                 .isActive(Math.round(Math.random() * 10) % 2 == 0)
                 .INN("" + Math.round(Math.random() * 123456789))
@@ -21,6 +24,28 @@ public class ClientServiceImpl implements ClientService {
         .build();
 
         return client;
+    }
+
+    public  String getRandPhotoPach() {
+
+
+        return "path";
+    }
+
+    public String getString() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)(random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+
+        return generatedString;
     }
 
     public void addClient(ClientDto dto) {
